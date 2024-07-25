fac0c657df05b1912e0bbd7f2c0b8e2541be260b
Mark Carpenter <Markcarpenter0025@gmail.com>
2024-07-24 23:38:19 -0500

    Updated environment variable loading in the Next.js configuration to use combined environment variables.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
+import { loadEnvConfig } from '@next/env'
+
+const { combinedEnv } = loadEnvConfig(process.cwd())
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-      NEXT_DEV_URL: process.env.NEXT_DEV_URL,
-    },
-  }
-  
-  export default nextConfig
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: combinedEnv.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: combinedEnv.CLERK_SECRET_KEY,
+    NEXT_DEV_URL: combinedEnv.NEXT_DEV_URL,
+  },
+}
+
+export default nextConfig
   
